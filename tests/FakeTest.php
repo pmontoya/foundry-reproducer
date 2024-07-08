4cb08b65bac78fa18b4fff15251f3f0a91f93b47
Pascal Montoya <pascal.montoya@gmail.com>
2024-07-08 11:12:00 +0200

    Composer update

--- tests/FakeTest.php
+++ tests/FakeTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
-use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
